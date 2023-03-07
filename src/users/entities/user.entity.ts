8320dc3e194579c43ccf46af3214b40a5ad79342
Cyril Clavere <cyril.clavere@frog.co>
2023-03-07 14:57:51 +0100

    first commit users

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,12 @@
+export class User {
+    _id?: string;
+    name: string;
+    lastName: string;
+    nickname?: string;
+    email: string;
+    birthday: Date;
+    age: number;
+    password: string;
+    lastModification: Date;
+    lastConnexion: Date;
+}
